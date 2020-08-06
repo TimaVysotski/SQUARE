a7376b5c94748cc586ac954feedc7d951ef9c420
Timan <vysotski.tima@gmail.com>
2020-08-06 11:12:37 +0300

    SQUARE Opacity bug fix

--- src/container/components/view/Card.tsx
+++ src/container/components/view/Card.tsx
@@ -17,7 +17,6 @@ const StyledCard = styled(View)`
   flex: ${({ flex = 1 }: CardProps): number => Number(flex)};
   ${({ color }: CardProps): string => `backgroundColor: ${color}`}
   border-radius: ${sizes.baseBorderRadius};
-  opacity: 0.8;
   margin-top: ${({ mt = 0 }: CardProps): number => Number(mt) * sizes.baseMargin};
   margin-bottom: ${({ mb = 0 }: CardProps): number => Number(mb) * sizes.baseMargin};
   margin-right: ${({ mr = 0 }: CardProps): number => Number(mr) * sizes.baseMargin};
